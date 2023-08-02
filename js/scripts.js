85926958fc467e8bc35fd17c248780b35e269d58
Emily <emilyonishi93@gmail.com>
2023-08-02 14:24:09 -0700

    add new titles n functions that will change the msgs on ur screen o.o

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const form = document.querySelector("form");
+    form.onsubmit = function(event){
+        event.preventDefault();
+
+        let startMsg = document.getElementById("start");
+        startMsg.setAttribute("class", "hidden");
+        
+        let nxtMsg = document.getElementById("nxt");
+        nxtMsg.removeAttribute("class");
+    }
+}
+
