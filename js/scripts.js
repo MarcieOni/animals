c0dfde62faf82c461c6009c911df3c999da93cd0
Emily <emilyonishi93@gmail.com>
2023-08-03 09:40:05 -0700

    add pics of all the animals n made it so u can search 4 them on the search box o.o

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,35 @@ window.onload = function() {
         let startMsg = document.getElementById("start");
         startMsg.setAttribute("class", "hidden");
         let nxtMsg = document.getElementById("nxt");
-        nxtMsg.removeAttribute("class");
-        const animal = document.getElementById("animal");
+        const animal = document.getElementById("animal").value;
+        const monkeyPic = document.getElementById("monkey");
+        const dogPic = document.getElementById("dog");
+        const catgirlPic = document.getElementById("catgirl");
+        const fishPic = document.getElementById("fish");
+        const lizardPic = document.getElementById("lizard");
+        const failMsg = document.getElementById("fail");
+        if(animal === "monkey") {
+            monkeyPic.removeAttribute("class", "hidden");
 
-        if(animal === monkey) {
-            
+        } else if(animal === "dog") {
+            dogPic.removeAttribute("class", "hidden");
+            nxtMsg.removeAttribute("class");
+
+        } else if(animal === "catgirl") {
+            catgirlPic.removeAttribute("class", "hidden");
+            nxtMsg.removeAttribute("class");
+
+        } else if(animal === "fish") {
+            fishPic.removeAttribute("class", "hidden");
+            nxtMsg.removeAttribute("class");
+
+        } else if(animal === "lizard") {
+            lizardPic.removeAttribute("class", "hidden");
+            nxtMsg.removeAttribute("class");
+
+        } else {
+            failMsg.removeAttribute("class", "hidden");
+
         }
-
     }
 }
